e09ecf5b2b435f5a4f769c9b9d3fa1ccc8305e4c
Cody Poll <cjpoll@gmail.com>
2016-10-20 17:50:08 -0600

    Add channel/2 and channel/3 functions
    channel/2 allows you to pass in arguments to an anonymous function. The
    channel is prepended to this list before applying.
    
    channel/3 allows you to pass in an MFA. The channel is prepended to the
    args list befre applying.
    
    Also, general cleanup.

--- lib/subscribex.ex
+++ lib/subscribex.ex
@@ -16,39 +16,63 @@ defmodule Subscribex do
   @spec channel(:link | :no_link | :monitor)
   :: %AMQP.Channel{} | {%AMQP.Channel{}, monitor}
   def channel(link) when is_atom(link) do
-    connection_name = Application.get_env(:subscribex, :connection_name, Subscribex.Connection)
-    connection_pid = Process.whereis(connection_name)
-
-    if connection_pid do
-      connection = %AMQP.Connection{pid: connection_pid}
-      {:ok, channel} = AMQP.Channel.open(connection)
-
-      case link do
-        :link ->
-          Process.link(channel.pid)
-          channel
-        :no_link -> channel
-        :monitor ->
-          monitor = Process.monitor(channel.pid)
-          {channel, monitor}
-      end
-    else
-      30
-      |> :timer.seconds
-      |> :timer.sleep
-
-      channel(link)
-    end
+    :subscribex
+    |> Application.get_env(:connection_name, Subscribex.Connection)
+    |> Process.whereis
+    |> do_channel(link)
   end
 
-  def channel(callback) when is_function(callback, 1) do
+  def channel(callback, args \\ []) when is_function(callback, 1) do
     channel = Subscribex.channel(:link)
-    callback.(channel)
+    args = [channel | args]
+    result = apply(callback, args)
     Subscribex.close(channel)
+
+    result
+  end
+
+  def channel(module, function, args)
+  when is_atom(module)
+  and is_atom(function)
+  and is_list(args) do
+    channel = Subscribex.channel(:link)
+    args = [channel | args]
+    result = apply(module, function, args)
+    Subscribex.close(channel)
+
+    result
   end
 
   @spec close(channel) :: :ok | :closing
   def close(%AMQP.Channel{} = channel) do
     AMQP.Channel.close(channel)
+  end
+
+  ## Private Functions
+
+  defp apply_link(%AMQP.Channel{} = channel, :no_link), do: channel
+
+  defp apply_link(%AMQP.Channel{} = channel, :monitor) do
+    monitor = Process.monitor(channel.pid)
+    {channel, monitor}
+  end
+
+  defp apply_link(%AMQP.Channel{} = channel, :link) do
+    Process.link(channel.pid)
+    channel
+  end
+
+  defp do_channel(nil, link) do
+    30 # TODO: Make this configurable
+    |> :timer.seconds
+    |> :timer.sleep
+
+    channel(link)
+  end
+
+  defp do_channel(connection_pid, link) when is_pid(connection_pid) do
+    connection = %AMQP.Connection{pid: connection_pid}
+    {:ok, channel} = AMQP.Channel.open(connection)
+    apply_link(channel, link)
   end
 end
